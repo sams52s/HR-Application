3127b3f53aad0a386186bb623f3e682784c12e75
sams52s <sams52tas@gmail.com>
2025-09-11 20:01:23 +0600

    update request mapping for EmployeeController to use /api/employees

--- backend/src/main/java/com/leads/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/leads/backend/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hr/employees")
+@RequestMapping("/api/employees")
 public class EmployeeController {
   private final EmployeeService employeeService;
 
